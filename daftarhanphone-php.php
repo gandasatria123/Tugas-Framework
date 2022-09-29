ae0f667b4a6e6c3b473b9f387d048719898493e3
gandasatria123 <114660799+gandasatria123@users.noreply.github.com>
2022-09-29 20:05:21 +0700

    Add files via upload

---
+++ daftarhanphone-php.php
@@ -0,0 +1,43 @@
+<html>
+    <head>
+        <title> Daftar Hanphone </title>
+</head>
+<body>
+    <h1>Daftar Handphone</h1>
+    <img src="A52s.jpg" heght="50px" width="70px;"> 
+</body>
+</html>
+<?php
+$nama    = "Samsung A52s"; 
+$no      = 101; 
+$nama1 = "Ram 8 GB / Internal 128 GB"; 
+$nama2 ="Gratis Ongkir (COD / antar ke-rumah)";
+
+echo "<b> Rp5.000.000,00</b> <br><br>";
+echo "Nama barang: ".$nama."<br>";
+echo "Code barang: ".$no."<br>";
+
+
+print "Spesifikasi : ".$nama1."<br>";
+print "Pengiriman : ".$nama2."<br>";
+//print "desimal : ".$desimal."<br>";
+?>
+
+
+</br></br><img src="xr.png" height="50px" width="80px;">
+<?php
+$nama    = "Iphone XR"; 
+$no      = 102; 
+$nama1 = "Ram 8 GB / Internal 128 GB"; 
+$nama2 ="Gratis Ongkir (COD / antar ke-rumah)";
+
+echo "<b> Rp4.800.000,00</b> <br><br>";
+echo "Nama barang: ".$nama."<br>";
+echo "Code barang: ".$no."<br>";
+
+
+print "Spesifikasi : ".$nama1."<br>";
+print "Pengiriman : ".$nama2."<br>";
+//print "desimal : ".$desimal."<br>";
+?>
+
